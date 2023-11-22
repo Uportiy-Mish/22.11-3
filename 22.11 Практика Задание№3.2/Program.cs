4a529b3045077e5a5504c78acb38e1ccbeab65ae
Asus <Asus@DESKTOP-Q1VVDVK>
2023-11-22 13:34:23 +0300

    Добавьте файлы проекта.

---
+++ 22.11 Практика Задание№3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _22._11_Практика_Задание_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1;
+            name1 = Interaction.InputBox("Введите день недели",
+                "Ввод дня недели");
+            string name2;
+            name2 = Interaction.InputBox("Введите номер дня недели",
+                "Ввод номера недели");
+            string name3;
+            name3 = Interaction.InputBox("Введите дату",
+                "Ввод даты");
+            string txt = "День недели: " + name1 + ", Номер дня недели: " + name2 + ", Дата: " + name3;
+            MessageBox.Show(txt, "Итог.");
+        }
+    }
+}
---
+++ 22.11 Практика Задание№3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11_Практика_Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите день недели.");
+            string name1 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Введите номер дня недели.");
+            string name2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Введите дату.");
+            string name3 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"День недели: {name1}, Номер дня недели: {name2}, Дата: {name3}");
+            Console.ReadKey();
+
+        }
+    }
+}
